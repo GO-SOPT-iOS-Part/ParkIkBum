06800607552aa24911d0dc5803cbe95df0198be3
parkikbum <pibum@naver.com>
2023-04-15 15:35:55 +0900

    add: initialize proj

---
+++ Clone-Coding/Tving-Clone-Coding/Tving-Clone-Coding/Source/VCs/Login/LoginVC.swift
@@ -0,0 +1,12 @@
+import UIKit
+import SnapKit
+import Then
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .green
+
+    }
+}
