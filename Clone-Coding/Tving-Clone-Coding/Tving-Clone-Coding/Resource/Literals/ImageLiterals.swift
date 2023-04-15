5e0f954b4fc12b5605371bcc71bfd4a73bf94cf8
parkikbum <pibum@naver.com>
2023-04-15 16:39:01 +0900

    add: add imageLiterals

--- Clone-Coding/Tving-Clone-Coding/Tving-Clone-Coding/Resource/Literals/ImageLiterals.swift
+++ Clone-Coding/Tving-Clone-Coding/Tving-Clone-Coding/Resource/Literals/ImageLiterals.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 enum ImageLiterals {
-    static var icFollowing: UIImage { .load(name: "icBack") }
+    static var icBack: UIImage { .load(name: "icBack") }
     static var icEye: UIImage { .load(name: "icEye") }
     static var icEyeSlash: UIImage { .load(name: "icEyeSlash") }
     static var icXCircle: UIImage { .load(name: "icXCircle") }
