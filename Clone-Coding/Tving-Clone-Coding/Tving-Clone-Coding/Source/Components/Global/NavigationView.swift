a06827be421ad8a1671c6831d255ec9391e98d92
parkikbum <pibum@naver.com>
2023-04-15 16:38:05 +0900

    feat: impletment custom navigation view

--- Clone-Coding/Tving-Clone-Coding/Tving-Clone-Coding/Source/Components/Global/NavigationView.swift
+++ Clone-Coding/Tving-Clone-Coding/Tving-Clone-Coding/Source/Components/Global/NavigationView.swift
@@ -3,8 +3,36 @@ import Then
 import SnapKit
 
 class NavigationView: UIView {
+    var navigationController: UINavigationController?
+    
     required init?(coder: NSCoder) {
         super.init(coder: coder)
     }
     
+    init(navigationController: UINavigationController) {
+        super.init(frame: .zero)
+        self.navigationController = navigationController
+    }
+    
+    private func setLayout() {
+        self.addSubview(backButton)
+        backButton.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview().inset(ScreenUtils.getWidth(24))
+            $0.width.height.equalTo(ScreenUtils.getWidth(24))
+        }
+    }
+    
+    private lazy var backButton = UIButton().then {
+        $0.setImage(ImageLiterals.icBack, for: .normal)
+        $0.imageView?.contentMode = .scaleAspectFill
+        $0.addTarget(self,
+                     action: #selector(didBackButtonTapped),
+                     for: .touchUpInside)
+    }
+    
+    @objc private func didBackButtonTapped() {
+        self.navigationController?.popViewController(animated: true)
+    }
 }
+
